177220f18a3d5d028e6849fdeb97259df80c9f7e
Moid Hassan <has.moid@gmail.com>
2022-02-13 16:51:22 +0530

    solution to exercise 2

---
+++ nn_viz_02.py
@@ -0,0 +1,46 @@
+"""
+Generate a autoencoder neural network visualization
+"""
+
+
+#setting up constant params
+FIGURE_WIDTH = 16
+FIGURE_HEIGHT = 9
+RIGHT_BORDER = 0.7
+LEFT_BORDER = 0.7
+TOP_BORDER = 0.8
+BOTTOM_BORDER = 0.6
+
+N_IMAGE_PIXEL_COLS = 64
+N_IMAGE_PIXEL_ROWS = 48
+N_NODES_BY_LAYER = [10,7,5,8]
+
+INPUT_IMAGE_BOTTOM = 5
+INPUT_IMAGE_HEIGHT = 0.25 * FIGURE_HEIGHT
+ERROR_IMAGE_SCALE = 0.7
+ERROR_GAP_SCALE = 0.3
+BETWEEN_LAYER_SCALE = 0.8 
+BETWEEN_NODE_SCALE = 0.4
+
+
+def main():
+    #print("All the visualization code goes here")
+    print(f"Node images are {N_IMAGE_PIXEL_ROWS}" + f" by {N_IMAGE_PIXEL_COLS} pixels")
+    print(f"Figure width is {FIGURE_WIDTH} and figure height is {FIGURE_HEIGHT}")
+    print(f"Figure width is {FIGURE_WIDTH} and figure height is {FIGURE_HEIGHT}")
+    p = construct_parameters()
+    print("params:")
+    print(p)
+
+def construct_parameters():
+    """
+    Build a dictionary of params that describe the size and location 
+    of the elements of visualization.
+    """
+    params = {}
+    params["figure"] = {"height":FIGURE_HEIGHT, "width":FIGURE_WIDTH}
+    return params
+
+
+if __name__ == "__main__":
+    main()
